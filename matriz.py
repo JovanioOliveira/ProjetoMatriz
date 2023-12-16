fbe42d14203c819882eaecf7f8cad23b4b976c70
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2023-12-15 23:03:12 -0300

    Criando matriz com entrada usuário

---
+++ matriz.py
@@ -0,0 +1,15 @@
+matrizes = []
+
+numLinhas = int(input('Informe o número de Linhas: '))
+numColunas = int(input('Infrome o número de Colunas: '))
+
+m = []
+
+for i in range(numLinhas):
+    linhas = []
+    for j in range(numColunas):
+        elemento = int(input(f'm{[i]}{[j]}: '))
+        linhas.append(elemento)
+    m.append(linhas)
+matrizes.append(m)
+print(matrizes)

09a3e06fd1e9da7ab9e4e63967437b0d3a54f7ae
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2023-12-16 00:22:20 -0300

    Gerando mais de uma matriz e adicionando na matriz principal

--- matriz.py
+++ matriz.py
@@ -1,15 +1,20 @@
 matrizes = []
 
-numLinhas = int(input('Informe o número de Linhas: '))
-numColunas = int(input('Infrome o número de Colunas: '))
+while True:
+    numLinhas = int(input('Informe o número de Linhas: '))
+    numColunas = int(input('Infrome o número de Colunas: '))
 
-m = []
+    m = []
 
-for i in range(numLinhas):
-    linhas = []
-    for j in range(numColunas):
-        elemento = int(input(f'm{[i]}{[j]}: '))
-        linhas.append(elemento)
-    m.append(linhas)
-matrizes.append(m)
-print(matrizes)
+    for i in range(numLinhas):
+        linhas = []
+        for j in range(numColunas):
+            elemento = int(input(f'm{[i]}{[j]}: '))
+            linhas.append(elemento)
+        m.append(linhas)
+    matrizes.append(m)
+
+    entrada = input('Deseja continuar [S/N]: ').upper()
+    if entrada != 'S':
+        print(f'As matrizes informadas foram: \n{matrizes}')
+        break

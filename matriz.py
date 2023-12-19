a90350300a6cfdd1a4e206f1e1f0d0b290c92cb2
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2023-12-18 23:40:44 -0300

    Definindo duas matrizes para efetuar calculos

--- matriz.py
+++ matriz.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 matrizes = []
 
 while True:
@@ -12,12 +14,18 @@ while True:
             elemento = int(input(f'm{[i]}{[j]}: '))
             linhas.append(elemento)
         m.append(linhas)
-    matrizes.append(m)
-
+    #np.array. GARANTE QUE A MATRIZ SEJA UMA MATRIZ NA CONFIGURAÇÃO NUMPY
+    matrizes.append(np.array(m))                            
     entrada = input('Deseja continuar [S/N]: ').upper()
-    if entrada != 'S':
-        print(f'As matrizes informadas foram: \n{matrizes}')
 
+    if entrada != 'S':
+        print(f'As matrizes informadas foram: ')
         for i, matriz in enumerate(matrizes):
-            print(f'A matriz na posição {i}: \n{matrizes}')
+            print(f'A matriz na posição {i}: \n{matriz}')
         break
+primeiraEntrada = int(input('Informe uma posição para definir MATRIZ A: '))
+A = matrizes[primeiraEntrada]
+segundaEntrada = int(input('Informe uma posição para definir a MATRIZ B: '))
+B = matrizes[segundaEntrada]
+
+print(f'MATRIZ A:\n\n{A}\n\nMATRIZ B:\n\n{B}\n')

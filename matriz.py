8b41ed58c372ade15be8bfe6648cae61313494a8
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2024-01-01 13:35:08 -0300

    Definindo funções e Menu

--- matriz.py
+++ matriz.py
@@ -2,64 +2,147 @@ import numpy as np
 
 matrizes = []
 
-while True:
-    numLinhas = int(input('Informe o número de Linhas: '))
-    numColunas = int(input('Infrome o número de Colunas: '))
+# Tratamento para inserir número linhas e número de coluna válido
 
-    if numLinhas <= 0 or numColunas <=0:
-        print('\nPara gerar uma MATRIZ, inicie inserindo a partir do (Número 1),\n'
-              'para Número de Linhas e Número de Colunas\n') 
+def numeroInteiro(mensagem):
+    while True:
+        try:
+            numero = int(input(mensagem))
+            if numero <= 0 or numero == -0:
+                print('Favor insira um número inteiro maior que zero')
+                continue
+            return numero
+        except ValueError:
+            print('Favor insira um número inteiro válido.')
 
-    m = []
+def cadastrarMatriz():
+    while True:
+        # numLinhas = int(input('Informe o número de Linhas: '))
+        # numColunas = int(input('Infrome o número de Colunas: '))
 
-    for i in range(numLinhas):
-        linhas = []
-        for j in range(numColunas):
-            elemento = float(input(f'm{[i]}{[j]}: '))
-            linhas.append(elemento)
-        m.append(linhas)
+        numLinhas = numeroInteiro('\nInforme o número de Linhas: ')
+        numColunas = numeroInteiro('Informe número de Colunas: ')
+        print()
 
-    #np.array. GARANTE QUE A MATRIZ SEJA UMA MATRIZ NA CONFIGURAÇÃO NUMPY
-    
-    matrizes.append(np.array(m))                            
-    entrada = input('\nDeseja continuar [S/N]: ').upper()
+        # if numLinhas <= 0 or numColunas <=0:
+        #     print('\nPara gerar uma MATRIZ, inicie inserindo a partir do (Número 1),\n'
+        #         'para Número de Linhas e Número de Colunas\n')
 
-    if entrada != 'S':
-        print(f'\nAs matrizes informadas foram: ')
+        m = []
+
+        for i in range(numLinhas):
+            linhas = []
+            for j in range(numColunas):
+                while True:
+                    try:
+                        elemento = float(input(f'm{[i]}{[j]}: '))
+                        break
+                    except ValueError:
+                        print('Favor insira um número válido')
+                linhas.append(elemento)
+            m.append(linhas)
+
+        #np.array. GARANTE QUE A MATRIZ SEJA UMA MATRIZ NA CONFIGURAÇÃO NUMPY
+        
+        matrizes.append(np.array(m))
+
+        entrada = input('\nDeseja continuar [S/N]: ').upper()
+        if entrada != 'S':
+            print(f'\nMatriz(es) cadastradas com sucesso.\n')
+            break
+
+def listarMatrizes():
+        print('As Matrizes cadastradas foram:\n')
         for i, matriz in enumerate(matrizes):
             print(f'A matriz na posição {i}: {matriz.shape} \n{matriz}')
-        break
-primeiraEntrada = int(input('\nInforme uma posição para definir MATRIZ A: '))
-A = matrizes[primeiraEntrada]
-segundaEntrada = int(input('Informe uma posição para definir  MATRIZ B: '))
-B = matrizes[segundaEntrada]
+
+def selecionarMatriz():
+    primeiraEntrada = int(input('\nInforme uma posição para definir MATRIZ A: '))
+    A = matrizes[primeiraEntrada]
+    segundaEntrada = int(input('Informe uma posição para definir  MATRIZ B: '))
+    B = matrizes[segundaEntrada]
+
+    return A, B
 
 # shape, Utiliza para ver Núm. Linhas e Núm.Colunas de um array, indica o tamanho de cada dimensão da matriz(m,n)
 # np.dot, É uma função do numpy para manipulação de array e matrizes. Realiza a multiplicação de matrizes...
 
 # Multiplicação entre as Matrizes
 
-# if A.shape[1] != B.shape[0]:
-#     print('\nO Número de colunas da primeira matriz é diferente do número de linhas da segunda matriz\n "A MULTIPLICAÇÃO NÃO É POSSÍVEL"\n')
-# else:
-#     resultado = np.dot(A,B)
-#     print(f'\nA multiplicação entre as MATRIZES A * B : {resultado.shape} \n{resultado}\n')
+def multiplicacaoMatrizes(A, B):
+    if A.shape[1] != B.shape[0]:
+        print('\nO Número de colunas da primeira matriz é diferente do número de linhas da segunda matriz\n "A MULTIPLICAÇÃO NÃO É POSSÍVEL"\n')
+    else:
+        print(f'Matriz A = {A.shape}: \n{A}\n')
+        print(f'Matriz B = {B.shape}: \n{B}\n')
+
+        resultado = np.dot(A,B)
+        print(f'\nA multiplicação entre as MATRIZES A * B : {resultado.shape} \n{resultado}\n')
 
 # Soma entre as matrizes
 
-# if A.shape != B.shape:
-#     print('\As matrizes precisam ter o mesmo número de linhas e coluans para serem somadas')
-# else:
-#     soma = A + B
-#     print(f'O Resultado da SOMA entre as matrizes A + B: \n{soma}') 
+def somaMatrizes(A, B):
+    if A.shape != B.shape:
+        print('\As matrizes precisam ter o mesmo número de linhas e colunas para serem somadas')
+    else:
+        print(f'Matriz A = {A.shape}: \n{A}\n')
+        print(f'Matriz B = {B.shape}: \n{B}\n')
+
+        soma = A + B
+        print(f'O Resultado da SOMA entre as matrizes A + B: {soma.shape} \n{soma}') 
 
 # Subtração entre as atrizes
 
-if A.shape != B.shape:
-    print('\nAs matrizes precisam ter o mesmo níumero de linhas e colunas para serem substraídas')
-else:
-    subtracao = A - B
-    print(f'O Resultado da SUBTRAÇÃO entre as matrizes A - B: \n{subtracao}')
+def subtracaoMatrizes(A,B):
+    if A.shape != B.shape:
+        print('\nAs matrizes precisam ter o mesmo níumero de linhas e colunas para serem substraídas')
+    else:
+        print(f'Matriz A = {A.shape}: \n{A}\n')
+        print(f'Matriz B = {B.shape}: \n{B}\n')
 
+        subtracao = A - B
+        print(f'O Resultado da SUBTRAÇÃO entre as matrizes A - B: {subtracao.shape} {subtracao}')
 
-   
+# Definição de Menu
+        
+def menu():
+    while True:
+        print()
+        print(10*('=-='))
+        print(f'MENU'.center(30))
+        print(10*('=-='))
+
+        print('\n1. Cadastrar Matriz')
+        print('2. Listar Matrizes')
+        print('3. Multiplicação de Matrizes')
+        print('4. Soma de Matrizes')
+        print('5. Subtração de Matrizes')
+        print('6. Encerrar Programa\n')
+
+        print(10*('-=-'))
+
+        esolha = input('\nEscolha uma das opções: ')
+        if esolha == '1':
+            cadastrarMatriz()
+        elif esolha == '2':
+            listarMatrizes()
+        elif esolha == '3':
+            A, B = selecionarMatriz()
+            multiplicacaoMatrizes(A, B)
+        elif esolha == '4':
+            A, B = selecionarMatriz()
+            somaMatrizes(A, B)
+        elif esolha == '5':
+            A, B = selecionarMatriz()
+            subtracaoMatrizes(A, B)
+        elif esolha == '6':
+            print('Programa Encerrado.\n')
+            break
+        else:
+            print('Opção Inválida. Tente novamente')
+       
+
+if __name__ == '__main__':
+    menu()
+
+ 

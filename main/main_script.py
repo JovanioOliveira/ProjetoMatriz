d763c475f57d99b9d53a1e345918a03c4473a828
JovanioOliveira <135073526+JovanioOliveira@users.noreply.github.com>
2025-11-07 10:47:37 -0300

    Atualiza main_script.py com novas modificações

--- main/main_script.py
+++ main/main_script.py
@@ -1,7 +1,4 @@
-import sys
-# SUBSTITUA PARA O CAMINHO QUE ESTÁ NO DIRETÓRIO DO SEU PC
-sys.path.append("C:\\Users\\jcj_o\\OneDrive\\Documentos\\IFPI - Picos\\IFPI - 2023 - ANALISE E DESENVOLVIMENTO SISTEMA\\IFPI - 2023.2 - ANALISE_DESENVOLV\\SEGUNDO SEMESTRE\\PROG. ORIETADA OBJETO - Prof. Jader\\Projeto_Matrizes")
-
+# main/main_script.py
 from src.matriz import menu
 
 if __name__ == '__main__':
